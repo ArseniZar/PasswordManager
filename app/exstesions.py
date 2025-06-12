d0fd861d688df7bfe3aed823fc381f63830053ec
ArseniZar <arseni.zarkiewicz@gmail.com>
2025-06-12 14:31:46 +0200

    Refactor extensions.py: update and organize app extensions
    - Improved initialization and configuration of extensions
    - Cleaned up and optimized code for better maintainability

--- app/exstesions.py
+++ app/exstesions.py
@@ -1,3 +1,4 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
@@ -5,7 +6,7 @@ from flask_mail import Mail
 from flask_caching import Cache
 from itsdangerous import URLSafeTimedSerializer
 
-
+SESSION_TOKEN_KEY_PATTERN = "token_from_password_{}"
 
 
 db = SQLAlchemy()
@@ -16,7 +17,7 @@ cache = Cache()
 
 serializer = None 
 
-def init_serializer(app):
+def init_serializer(app: Flask):
     global serializer
     serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
     
